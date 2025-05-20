5fbba278feba4c04fb6dc278c74ea256571d9cd4
GhulamAkber368 <GhulamAkber368@gmail.com>
2025-05-20 13:54:16 +0500

    Learning Integration Testing in Flutter

---
+++ lib/integration_testing/app_test.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:integration_test/integration_test.dart";
+import "package:integration_testing/home_view.dart";
+import 'package:integration_testing/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group("Integration Testing or End to End Testing", () {
+    testWidgets("Verify Login Screen with correct Email and Password",
+        (tester) async {
+      // To ensure app main func called and this file main shouldnt call therefore we imported main as app;
+      app.main();
+      await tester.pumpAndSettle();
+      await tester.enterText(find.byType(TextField).at(0), "Email");
+      await tester.enterText(find.byType(TextField).at(1), "Password");
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomeView), findsOneWidget);
+    });
+
+    testWidgets("Verify Login Screen with uncorrect Email or Password",
+        (tester) async {
+      // To ensure app main func called and this file main shouldnt call therefore we imported main as app;
+      app.main();
+      await tester.pumpAndSettle();
+      await tester.enterText(find.byType(TextField).at(0), "Emails");
+      await tester.enterText(find.byType(TextField).at(1), "Password");
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(AlertDialog), findsOneWidget);
+    });
+  });
+}
--- lib/login_view.dart
+++ lib/login_view.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:integration_testing/home_view.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  TextEditingController emailTEContr = TextEditingController();
+  TextEditingController passTEContr = TextEditingController();
+
+  login() {
+    if (emailTEContr.text.trim() == "Email" &&
+        passTEContr.text.trim() == "Password") {
+      Navigator.push(
+          context, MaterialPageRoute(builder: ((cxt) => const HomeView())));
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext cxt) {
+            return AlertDialog(
+              title: const Text("Error"),
+              content: const Text("Invalid Email or Password"),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("Ok"))
+              ],
+            );
+          });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailTEContr.dispose();
+    passTEContr.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +50,21 @@ class LoginView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
-              decoration: InputDecoration(label: Text("Email")),
+            TextField(
+              controller: emailTEContr,
+              decoration: const InputDecoration(label: Text("Email")),
             ),
             const SizedBox(
               height: 10,
             ),
-            const TextField(
-              decoration: InputDecoration(label: Text("Password")),
+            TextField(
+              controller: passTEContr,
+              decoration: const InputDecoration(label: Text("Password")),
             ),
             const SizedBox(
               height: 20,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: ((cxt) => const HomeView())));
-                },
-                child: const Text("Login"))
+            ElevatedButton(onPressed: login, child: const Text("Login"))
           ],
         ),
       )),
